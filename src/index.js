c9a89a3e87445fbe0aefcbe2581ea05de7311c35
Jhankda <harshjhankda200988@gmail.com>
2024-04-08 22:42:08 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
 
+import {app} from './app.js'
+
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`serevr is running at ${process.env.PORT || 8000}`)
+    })
+    
+})
+.catch((err) => {
+    console.log("MongoDB connection failed :",err)
+})
 
 
 
