9bb267add6078dc0914f31a2cdbd051c629a2ecc
Jhankda <harshjhankda200988@gmail.com>
2024-04-13 11:57:10 +0530

    upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+
+
+
+          
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadCloudinary  = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // Upload a file to cloudinary
+        const response  = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        }
+        )
+        console.log(response.url)
+        return response;;
+
+    } catch(error){
+        fs.unlinkSync(localFilePath) //remove local temp file to be uploaded in case it is malicious
+        return null;
+
+
+    }
+}
+
+
+
+export {uploadCloudinary}
+// // Upload a file to cloudinary
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
