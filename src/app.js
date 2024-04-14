21f57192fcd7720fd5537888347751b243802173
Jhankda <harshjhankda200988@gmail.com>
2024-04-14 14:43:30 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -15,5 +15,16 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+
+
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
+
+
 
 export { app }
